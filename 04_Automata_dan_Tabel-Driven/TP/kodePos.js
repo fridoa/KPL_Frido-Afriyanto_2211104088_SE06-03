0610e427508cd3133694cab85e33760e1e79da1c
fridoa <fridoafriyanto3@gmail.com>
2025-03-23 15:33:55 +0700

    menambahkan implementasi dengan table-driven

---
+++ 04_Automata_dan_Tabel-Driven/TP/kodePos.js
@@ -0,0 +1,24 @@
+class KodePos {
+  static kodePos = {
+    Batununggal: 40266,
+    Kujangsari: 40287,
+    Mengger: 40267,
+    Wates: 40256,
+    Cijaura: 40287,
+    Jatisari: 40286,
+    Margasari: 40286,
+    Sekejati: 40286,
+    Kebonwaru: 40272,
+    Maleer: 40274,
+    Samoja: 40273,
+  };
+
+  static getKodePos(kelurahan) {
+    return this.kodePos[kelurahan] || "Kode pos tidak ditemukan";
+  }
+}
+
+// Contoh pemanggilan
+console.log("Kode Pos Batununggal:", KodePos.getKodePos("Batununggal"));
+console.log("Kode Pos Cijaura:", KodePos.getKodePos("Cijaura"));
+console.log("Kode Pos Sokaraja:", KodePos.getKodePos("Sokaraja"));
